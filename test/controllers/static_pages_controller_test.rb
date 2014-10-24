386034cc7467b064c7a546aa429d647ed2c2c472
RonLeapYearBoy <RonLeapYearBoy@yahoo.com>
2014-10-23 22:56:25 -0400

    added Static Pages Controller test rb and test_helper rb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,47 @@
+# --------------static_pages_controller_test.rb ------------------------------------ #
+# ---        09.000-Static-Pages-Controller-Test.txt                             --- #
+# ---------------------------------------------------------------------------------- #
+# ---                                                                            --- #
+# 3.4.1 Testing titles (Red)                                                         #
+# https://www.railstutorial.org/book/static_pages#sec-testing_titles                 #
+#                                                                                    #
+# Listing 3.22: The Static Pages controller test with title tests.                   #
+# https://www.railstutorial.org/book/static_pages#code-custom_home_page              #
+#                                                                                    #
+#  gedit test/controllers/static_pages_controller_test.rb                            #
+#                                                                                    #
+# checks for the presence of a <title> tag containing the string                     #
+# "Home | Ruby on Rails Tutorial Sample App"                                         #
+# ---------------------------------------------------------------------------------- #
+
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get about" do
-    get :about
-    assert_response :success
-  end
 
-  test "should get contact" do
-    get :contact
+  test "should get home" do
+    get :home
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get home" do
-    get :home
+  test "should get about" do
+    get :about
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contactt" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
+# ---------------------------------------------------------------------------------- #
+
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,23 @@
+# ------------     Minitest_test_helper.txt      ----------------------------------- #
+# ---               09.000-Test_Helper.rb                                        --- #
+# ---------------------------------------------------------------------------------- #
+# ---                                                                            --- #
+# 3.7.1 MiniTest reporters                                                           #
+# https://www.railstutorial.org/book/static_pages#sec-static_pages_exercises         #
+#                                                                                    #
+# Listing 3.40: Configuring the tests to show red and green.                         #
+# https://www.railstutorial.org/book/static_pages#code-base_title_test               #
+#                                                                                    #
+#  gedit test/test_helper.rb                                                         #
+#                                                                                    #
+# test errors status in red when there are errors found                              #
+#                                                                                    #
+# ---------------------------------------------------------------------------------- #
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
